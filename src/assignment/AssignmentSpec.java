5f81277d9f3a77b3924e0ce5564551088c45213f
Trevon Harvey <twharv0002@ung.edu>
2017-01-21 13:01:19 -0500

    return results while typing

--- src/assignment/AssignmentSpec.java
+++ src/assignment/AssignmentSpec.java
@@ -25,7 +25,7 @@ public class AssignmentSpec {
 	}
 	
 	public boolean matches(AssignmentSpec searchSpec, DateOption dateOption, String gradeOption){
-		if(!(searchSpec.getName().equals("") || getName().toLowerCase().equals(searchSpec.getName().toLowerCase()))){
+		if(!(searchSpec.getName().equals("") || getName().toLowerCase().startsWith(searchSpec.getName().toLowerCase()))){
 			return false;
 		}
 		if(!(searchSpec.getClassName().equals("") || getClassName().equals(searchSpec.getClassName()))){
